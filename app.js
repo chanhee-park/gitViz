c5ad36d6d834cbed96737e6958343363bfe799a7
chanhee13p <chanhee13p@gmail.com>
2018-05-13 15:53:24 +0900

    add vue

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(sassMiddleware({
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
